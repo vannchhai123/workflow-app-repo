df3aba77d147ffec689c2ea51fdcbb2efddf760f
Vannchhai <vannchhai.sti@gmail.com>
2026-02-10 23:20:36 +0700

    formatted

--- app/page.tsx
+++ app/page.tsx
@@ -32,8 +32,8 @@ export default function Home() {
               Learning
             </a>{' '}
             center.
-             </p>
-                        </div>
+          </p>
+        </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="bg-foreground text-background flex h-12 w-full items-center justify-center gap-2 rounded-full px-5 transition-colors hover:bg-[#383838] md:w-[158px] dark:hover:bg-[#ccc]"
@@ -41,13 +41,13 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-                                      <Image
-                                        className="dark:invert"
-                                        src="/vercel.svg"
-                                        alt="Vercel logomark"
-                                        width={16}
-                                        height={16}
-                                      />
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
             Deploy Now
           </a>
           <a
